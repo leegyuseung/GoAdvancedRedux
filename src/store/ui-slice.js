a1c26482ab5da812c0fe1d9379346e3ce8c81d79
lee gyuseung <gyus0421@gmail.com>
2023-11-02 23:26:12 +0900

    리덕스로 Http State 및 피드백 처리

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -4,10 +4,18 @@ const uiSlice = createSlice({
   name: "ui",
   initialState: {
     cartIsVislbie: false, // 장바구니 표시여부
+    notification: null,
   },
   reducers: {
     toggle(state) {
       state.cartIsVislbie = !state.cartIsVislbie;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        state: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
